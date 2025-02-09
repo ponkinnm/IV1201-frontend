557824f4e77e4472f219428df093cc5a177e8f21
Pontus Kinnmark <82177239+ponkinnm@users.noreply.github.com>
2025-02-09 18:23:38 +0100

    fix: remove some unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import './App.css';
-import ListApplicants from './ListApplicants.tsx';
+import ListApplicants from './pages/ListApplicants.tsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
+import Header from './components/Header.tsx';
 
 function App() {
   return (
     <>
       <CssBaseline />
       <BrowserRouter>
+        <Header />
         <Routes>
-          <Route path="/" />
-          <Route path="/applicants" element={<ListApplicants />} />
+          <Route path="/" element={<ListApplicants />} />
         </Routes>
       </BrowserRouter>
     </>
