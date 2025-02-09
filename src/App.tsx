d20c46a491e27a22621b7c27b06132d2fb13dc51
Pontus Kinnmark <82177239+ponkinnm@users.noreply.github.com>
2025-02-09 18:18:20 +0100

    Add a router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css';
 import ListApplicants from './ListApplicants.tsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
 
 function App() {
   return (
     <>
       <CssBaseline />
-      <ListApplicants />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" />
+          <Route path="/applicants" element={<ListApplicants />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
