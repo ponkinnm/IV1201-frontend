50e6cd0de9e811a0e20fba6c4cbeea1edfb1e122
Pontus Kinnmark <82177239+ponkinnm@users.noreply.github.com>
2025-02-09 13:50:48 +0100

    Run prettier on project

--- prettier.config.js
+++ prettier.config.js
@@ -5,9 +5,8 @@
  * @type {import("prettier").Config}
  */
 const config = {
-  trailingComma: "es5",
+  trailingComma: 'es5',
   tabWidth: 2,
-  semi: false,
   singleQuote: true,
 };
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ListApplicants from './ListApplicants.tsx'
+import { CssBaseline } from '@mui/material'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          IV1201 Frontend
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CssBaseline />
+      <ListApplicants />
+      <div className="outer-container"></div>
     </>
   )
 }
