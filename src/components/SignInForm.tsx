79d006de331df10381c49ef0dbc787a9eeea1ac1
Benjamin <71153121+Befi99@users.noreply.github.com>
2025-02-28 16:28:07 +0100

    Update src/components/SignInForm.tsx
    Co-authored-by: Pontus Kinnmark <82177239+ponkinnm@users.noreply.github.com>

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -20,9 +20,7 @@ const Card = styled(MuiCard)(({ theme }) => ({
   maxWidth: 'min(450px, 90dvw)',
   padding: theme.spacing(4),
   gap: theme.spacing(2),
-  [theme.breakpoints.up('sm')]: {
-    maxWidth: '450px',
-  },
+marginTop: '25%',
   boxShadow: 'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
   ...theme.applyStyles('dark', {
     boxShadow: 'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
